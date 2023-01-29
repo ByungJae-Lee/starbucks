a8f0cf6dc25ef9c0a7d39768d71547fe3d250e54
banya <bjgeumgang@gmail.com>
2023-01-29 14:07:16 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEL = document.querySelector(".search");
-const searchInputEL = document.querySelector("input");
-
-searchEL.addEventListener("click", function () {
-  searchInputEL.focus();
-});
-
-searchInputEL.addEventListener("focus", function () {
-  searchEL.classList.add("focused");
-  searchInputEL.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEL.addEventListener("blur", function () {
-  searchEL.classList.remove("focused");
-  searchInputEL.setAttribute("placeholder", "");
-});
-
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector("#to-top");
 
@@ -141,6 +124,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear(); // 2023
